f2b49d846b6e5a6cfb98f9eb05f7a9090734a77e
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-26 16:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-   document.getElementById('calculate').addEventListener('click', function() {
-            const prices = document.querySelectorAll('.prices');
-            let total = 0;
+const getSumBtn = document.createElement("button");
+getSumBtn.append("Get Total Price");
+document.body.appendChild(getSumBtn);
+const getAllPrices=document.querySelectorAll(".price");
+let sum=0;
+const table=document.querySelector("table");
+const getSum = () => {
+//Add your code here
+	getAllPrices.forEach(ele=>sum+=parseInt(ele.innerText));
+    table.innerHTML+=`<tr id="ans">
+	<td>Total</td>
+	 <td>${sum}</td>
+	</tr>`
+};
 
-            prices.forEach(price => {
-                total += parseFloat(price.textContent);
-            });
-
-            let totalRow = document.createElement('tr');
-            totalRow.innerHTML = `<td><strong>Total Price</strong></td><td><strong>$${total.toFixed(2)}</strong></td>`;
-            document.getElementById('grocery-list').appendChild(totalRow);
-        });
+getSumBtn.addEventListener("click", getSum);
